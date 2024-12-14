2e30205a3869307c2dba82c4dc9837147a009782
sathurshanath <sathurshanath2021@gmail.com>
2024-12-14 12:23:43 +0530

    fix error

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand' //state management libraries
 import axios from 'axios'
 
-const API_URL = import.meta.env.Mode==="development" ? "http://localhost:3000/api/auth":`${process.env.CLIENT_URL}/api/auth`;
+const API_URL = `${process.env.CLIENT_URL}/api/auth`|| "http://localhost:3000/api/auth";
 
 
 axios.defaults.withCredentials = true;
